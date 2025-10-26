5f8eb72377cac6cf6e0614d57e96ab4764328618
Shirayuki39 <lorddemecrius83@proton.me>
2025-10-26 12:55:33 +0900

    mt6789-common: Sync with common tree
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- mt6789-common-vendor.mk
+++ mt6789-common-vendor.mk
@@ -135,6 +135,7 @@ PRODUCT_PACKAGES += \
     libmtkcutils \
     libmtkproperty \
     libmtkrillog \
+    libthha \
     libvcodec_oal \
     libvcodecdrv_header_stub \
     libverno \
@@ -152,7 +153,6 @@ PRODUCT_PACKAGES += \
     libmp4enc_xa.ca7 \
     librcs_interface \
     librcs_volte_core \
-    libthha \
     libvcodec_utility_plat.mt6789 \
     libvolte_core_shr \
     libvolte_xdmc_shr \
